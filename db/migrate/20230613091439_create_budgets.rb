e93a333e15f022e2c8259faef6807533d70af3c7
shaaibu <shaaibusuleiman@gamil.com>
2023-06-13 10:34:35 +0100

    create migrations

--- db/migrate/20230613091439_create_budgets.rb
+++ db/migrate/20230613091439_create_budgets.rb
@@ -3,7 +3,7 @@ class CreateBudgets < ActiveRecord::Migration[7.0]
     create_table :budgets do |t|
       t.string :name
       t.decimal :amount
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
