33499f5b719e3585b98d9cd0a4f694956ba360b0
shaaibu <shaaibusuleiman@gamil.com>
2023-06-15 03:29:00 +0100

    Implement tests for request

--- spec/requests/budget_spec.rb
+++ spec/requests/budget_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :request do
+RSpec.describe Budget, type: :request do
     include Devise::Test::IntegrationHelpers
   before(:each) do
     @user = User.create(name: 'Isaac', email: 'kaplan@gmail.com', password: '123456', password_confirmation: '123456')
@@ -12,27 +12,27 @@ RSpec.describe Group, type: :request do
     sign_in @user
    end
  
-   describe 'GET /group' do
+   describe 'GET /budgets' do
     it 'return http success' do
-        get user_groups_path(@user)
+        get user_group_budgets_path(@user, @group)
         expect(response).to have_http_status(200)
     end
 
-    it 'render index page groups' do
-        get user_groups_path(@user)
-        expect(response.body).to include('Categories')
+    it 'render index page for transactions' do
+        get user_group_budgets_path(@user, @group)
+        expect(response.body).to include('Transaction')
     end
    end
 
-   describe 'GET /group/new' do
+   describe 'GET /budgets/new' do
     it 'return http success ' do
-        get new_user_group_path(@user)
+        get new_user_group_budget_path(@user, @group)
         expect(response).to have_http_status(200)
     end
 
-    it 'render form to add group' do
-        get new_user_group_path(@user)
-        expect(response.body).to include('Create Category')
+    it 'render form to add transaction' do
+        get new_user_group_budget_path(@user, @group)
+        expect(response.body).to include('Create New Transaction')
         expect(response.body).to include('Sign out')
     end
    end
